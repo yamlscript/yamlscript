20c0de3da8d2ca3d01c62d0153af713276441474
Owen Young <theowenyoung@gmail.com>
2022-07-18 12:21:28 +0800

    add murstach

--- serve.ts
+++ serve.ts
@@ -1,3 +1,18 @@
 import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
+import Mustache from "https://esm.sh/mustache@4.2.0";
 
-serve((req: Request) => new Response("Hello World"));
+serve((req: Request) => {
+
+  const view = {
+    title: "Joe",
+    calc: function () {
+      return 2 + 4;
+    }
+  };
+  
+  const output = Mustache.render("{{title}} spends {{calc}}", view);
+  const response =new Response("Hello World"+output);
+
+  return response;
+
+});
