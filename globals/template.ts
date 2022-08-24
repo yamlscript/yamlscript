923b89001ccff25b34793bf082303e927e3a2b46
Owen Young <theowenyoung@gmail.com>
2022-08-24 10:37:09 +0800

    template support copy

--- globals/template.ts
+++ globals/template.ts
@@ -1,5 +1,7 @@
 import { fs, mustache, path } from "./deps.ts";
-
+import env from "./env.ts";
+import * as user from "./user.ts";
+import * as os from "./os.ts";
 export function render(templateString: string, data: Record<string, unknown>) {
   // @ts-ignore: mustache is not typed
   return mustache.render(templateString, data);
@@ -8,16 +10,20 @@ export function render(templateString: string, data: Record<string, unknown>) {
 export async function copy(
   fromFile: string,
   toFile: string,
-  data: Record<string, unknown>,
-  options: Deno.WriteFileOptions = {},
+  data: Record<string, unknown> = {},
 ) {
   // template
   const templateString = await Deno.readTextFile(fromFile);
-  const rendered = render(templateString, data);
+  const rendered = render(templateString, {
+    env,
+    user,
+    os,
+    ...data,
+  });
   // ensure dir exists
   await fs.ensureDir(path.dirname(toFile));
   // write file
   console.log("copy file to ", toFile);
 
-  await Deno.writeTextFile(toFile, rendered, options);
+  await Deno.writeTextFile(toFile, rendered);
 }
